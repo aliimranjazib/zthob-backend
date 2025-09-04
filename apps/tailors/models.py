f5afe1d67e667bb200dfbf6d77669164747f6a0f
Muhammad Imran Manzoor <170098801+aliimranjazib@users.noreply.github.com>
2025-09-04 17:16:16 +0500

    models

--- apps/tailors/models.py
+++ apps/tailors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class TailorProfile(models.Model):
+    user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,
+                              related_name='tailor_prpfile')
+    shop_name=models.CharField(max_length=100)
+    contact_number=models.CharField(max_length=20,null=True,blank=True)
+    establishment_year=models.PositiveIntegerField()
+    tailor_experience=models.PositiveIntegerField()
+    working_hours=models.JSONField(default=dict,blank=True,null=True)
+    address = models.TextField(max_length=250, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    shop_status=models.BooleanField(default=True)
