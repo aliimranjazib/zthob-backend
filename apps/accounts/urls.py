a816ba7ca259c13f777db9bc9d713555df913de4
Muhammad Imran Manzoor <170098801+aliimranjazib@users.noreply.github.com>
2025-09-24 23:27:45 +0500

    test deployment

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,13 +4,15 @@ from apps.accounts.views import (
     UserRegistrationView,
     UserLoginView,
     UserProfileView,
-    ChangePasswordView)
+    ChangePasswordView,
+    test_deployment)
 
 
 urlpatterns = [
     path('register/',UserRegistrationView.as_view(),name='user-register'),
     path('login/',UserLoginView.as_view(),name='user-login'),
     path('profile/',UserProfileView.as_view(),name='user-profile'),
-    path('change-password/',ChangePasswordView.as_view(),name='change-password')
+    path('change-password/',ChangePasswordView.as_view(),name='change-password'),
+    path('test-deployment/', test_deployment, name='test-deployment'),
     
 ]
