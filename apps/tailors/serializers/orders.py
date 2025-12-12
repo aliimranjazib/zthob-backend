469fd0914cb0088a510ef323f57eefaaf52b057b
Muhammad Imran Manzoor <aliemranjazib@gmail.com>
2025-12-13 00:56:53 +0500

    update

---
+++ apps/tailors/serializers/orders.py
@@ -0,0 +1,13 @@
+# apps/tailors/serializers/orders.py
+from rest_framework import serializers
+from apps.orders.models import Order
+
+
+class TailorUpdateOrderStatusSerializer(serializers.Serializer):
+    """Serializer for tailor updating order status"""
+    tailor_status = serializers.ChoiceField(
+        choices=[choice for choice in Order.TAILOR_STATUS_CHOICES if choice[0] != 'none'],
+        required=True
+    )
+    notes = serializers.CharField(required=False, allow_blank=True)
+
