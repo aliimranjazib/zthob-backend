3178392f8f811f9e71db31917483928a3e38392e
Muhammad Imran Manzoor <aliemranjazib@gmail.com>
2026-03-04 17:26:01 +0500

    fix30 (#173)

--- apps/tailors/services/__init__.py
+++ apps/tailors/services/__init__.py
@@ -1,3 +1,4 @@
 from .order_pdf import generate_order_pdf
+from .analytics import TailorAnalyticsService
 
-__all__ = ['generate_order_pdf']
+__all__ = ['generate_order_pdf', 'TailorAnalyticsService']
