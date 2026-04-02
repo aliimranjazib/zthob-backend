abbd77b7592a9473467c5a792af84674424b7765
Muhammad Imran Manzoor <aliemranjazib@gmail.com>
2026-04-02 18:59:38 +0500

    fix09

--- apps/tailors/views/tailor_pos.py
+++ apps/tailors/views/tailor_pos.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+
 from django.contrib.auth import get_user_model
 from django.db.models import Count, Max
 
