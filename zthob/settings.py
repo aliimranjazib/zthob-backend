3b2c70f393299ec57748f8537a6e983ff61a85a0
Muhammad Imran Manzoor <aliemranjazib@gmail.com>
2026-05-13 12:21:07 +0500

    data

--- zthob/settings.py
+++ zthob/settings.py
@@ -347,7 +347,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME" : timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME" : timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME" : timedelta(days=14),
     "ROTATE_REFRESH_TOKENS" : True,
     "BLACKLIST_AFTER_ROTATION": True, 
