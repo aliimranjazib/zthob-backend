d227d22c0bcccf53ea163114c30ba3b6bfd1bbc9
Muhammad Imran Manzoor <aliemranjazib@gmail.com>
2026-05-13 18:11:45 +0500

    v1.8 (#220)

--- apps/riders/serializers.py
+++ apps/riders/serializers.py
@@ -513,15 +513,16 @@ class RiderOrderListSerializer(serializers.ModelSerializer):
         # Determine effective role for labeling purposes
         if request.user.is_admin:
             user_role = 'ADMIN'
-        elif obj.tailor == request.user:
-            user_role = 'TAILOR'
         elif obj.rider == request.user:
             user_role = 'RIDER'
+        elif obj.tailor == request.user:
+            user_role = 'TAILOR'
         elif obj.customer == request.user:
             user_role = 'USER'
         else:
-            if request.user.is_tailor: user_role = 'TAILOR'
-            elif request.user.is_rider: user_role = 'RIDER'
+            # For unassigned orders, prioritize RIDER role if they have the profile
+            if request.user.is_rider: user_role = 'RIDER'
+            elif request.user.is_tailor: user_role = 'TAILOR'
             else: user_role = 'USER'
         
         # Build next available actions
